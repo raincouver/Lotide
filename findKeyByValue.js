a8844953bbf976f469a488f716cc4da1d7bacdda
Rain Zhu <rain1995zhu@gmail.com>
2023-03-08 21:45:06 -0800

    added two more test codes tofindKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -26,6 +26,8 @@ const bestTVShowsByGenre = {
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, 1), undefined);
