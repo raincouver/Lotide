bffac244fed4917963720feb1c2b83e003cd772e
Rain Zhu <rain1995zhu@gmail.com>
2023-03-09 16:58:11 -0800

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,70 @@
+// TESING IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected} ✅`);
+  } else if (actual == expected) {
+    console.log(`❌Assertion Failed: ${actual} is not the same type as ${expected} ❌`);
+  } else if (actual !== expected) {
+    console.log(`❌Assertion Failed: ${actual} != ${expected} ❌`);
+  }
+};
+
+// Instruction
+// Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+
+/* findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma" */
+
+// FUNCTION IMPLEMENTATION
+const findKey = function(object, callback) {
+  const results = [];
+  for (let item in object) {
+    if (callback(object[item])){
+      return item;
+    }
+  }
+  return undefined;
+}
+
+// TEST CODE
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 4 },
+  "Ora":       { stars: 6 },
+  "Akelarre":  { stars: 4 }
+}, x => x.stars === 4), "elBulli");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 1 }
+}, x => x.stars === 1), "Blue Hill");
