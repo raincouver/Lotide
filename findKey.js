9b15e13b318e66f38039a43fb6248f3967842a30
Rain Zhu <rain1995zhu@gmail.com>
2023-03-09 16:59:34 -0800

    added findKey.js another test case

--- findKey.js
+++ findKey.js
@@ -68,3 +68,12 @@ assertEqual(findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 1 }
 }, x => x.stars === 1), "Blue Hill");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 1 }
+}, x => x.stars === 5), undefined);
