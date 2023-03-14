3a5e2b9eef75b43eac05db76e55800a84e0e9128
Rain Zhu <rain1995zhu@gmail.com>
2023-03-13 20:22:39 -0700

    changed middleTest.js from expect.to.eql to assert.deepEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,6 @@
 // test/assertEqualTest.js
-const expect = require('chai').expect;
+// const expect = require('chai').expect;
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 
@@ -8,36 +9,36 @@ describe("#middle", () => {
   it("returns [] for [1]", () => {
     let actual = middle([1]);
     let expected = middle([]);
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it("returns [] for [1, 2]", () => {
     let actual = middle([1,2]);
     let expected = [];
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it("returns [2] for [1,2,3]", () => {
     let actual = middle([1,2,3]);
     let expected = [2];
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it("returns [2, 3] for [1,2,3]", () => {
     let actual = middle([1,2,3,4]);
     let expected = [2,3];
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it("returns [3] for [1,2,3,4,5]", () => {
     let actual = middle([1,2,3,4,5]);
     let expected = [3];
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 
   it("returns [3,4] for [1,2,3,4,5,6]", () => {
     let actual = middle([1,2,3,4,5,6]);
     let expected = [3,4];
-    expect(actual).to.eql(expected);
+    assert.deepEqual(actual, expected);
   });
 });
