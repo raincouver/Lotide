6840638f59ff31bbfa75bc6e528f2dab5ea73f47
Rain Zhu <rain1995zhu@gmail.com>
2023-03-13 20:26:44 -0700

    changed tailTest.js with assert.deepEqual

--- test/tailTest.js
+++ test/tailTest.js
@@ -8,6 +8,7 @@ describe('#tail', () => {
   it('An empty array should yield an empty array for its tail', () => {
     let words = [];
     let tails = tail(words);
+    assert.deepEqual(tails, []);
     assert.strictEqual(tails.length, 0);
     assert.strictEqual(words.length, 0);
   });
@@ -15,13 +16,15 @@ describe('#tail', () => {
   it('An array with only one element should yield an empty array for its tail', () => {
     let words = ['Yo Yo'];
     let tails = tail(words);
+    assert.deepEqual(tails, []);
     assert.strictEqual(tails.length, 0);
     assert.strictEqual(words.length, 1);
   });
 
-  it('should be returning a new array and not modify the original array that is passed in', () => {
+  it("should be returning a new array and not modify the original array that is passed in; and return ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
     let words = ['Yo Yo', 'Lighthouse', 'Labs'];
     let tails = tail(words);
+    assert.deepEqual(tails, ['Lighthouse', 'Labs']);
     assert.strictEqual(tails.length, 2);
     assert.strictEqual(words.length, 3);
   });
